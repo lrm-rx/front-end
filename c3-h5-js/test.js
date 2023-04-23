63dd8f53b022b55fcf97e39980a5430fa214b494
lrm <756131502@qq.com>
2023-04-23 19:14:09 +0800

    学习记录-4.23

--- c3-h5-js/test.js
+++ c3-h5-js/test.js
@@ -19,3 +19,23 @@ function fn() {
 }
 
 print(fn);
+
+function User() {}
+User.prototype.sayHello = function () {};
+var u1 = new User();
+var u2 = new User();
+console.log(u1.sayHello === u2.sayHello); //  true
+console.log(u1.__proto__ === u1.constructor.prototype); //  true
+console.log(u1.constructor.prototype === User.prototype); //  true
+console.log(u1.__proto__ === User.prototype); //  true
+console.log(User.constructor); // Function
+console.log(User.prototype.constructor === User); // true
+console.log(User.prototype.constructor === u1.__proto__.constructor); // true
+console.log(User.prototype === Function.prototype); // false
+console.log(User.__proto__ === Function.prototype); // true
+console.log(User.__proto__ === Function.__proto__); // true
+console.log(u1.__proto__ === u2.__proto__); // true
+console.log(u1.__proto__ === User.__proto__); // false
+console.log(Function.__proto__ === Object.__proto__); // true
+console.log(Function.prototype.__proto__ === Object.prototype.__proto__); // false
+console.log(Function.prototype.__proto__ === Object.prototype); // true
