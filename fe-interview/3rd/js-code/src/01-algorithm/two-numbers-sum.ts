a63262f9716cc820d6b17af4a158ad5d776fd99e
lrm <756131502@qq.com>
2023-07-16 00:43:22 +0800

    两数之和

--- fe-interview/3rd/js-code/src/01-algorithm/two-numbers-sum.ts
+++ fe-interview/3rd/js-code/src/01-algorithm/two-numbers-sum.ts
@@ -1,4 +1,5 @@
 // 两数之和
+// 查找和为 n 的两个数 方法1
 
 export function findTowNumbers1(arr: number[], n: number): number[] {
     const res: number[] = [];
@@ -21,6 +22,37 @@ export function findTowNumbers1(arr: number[], n: number): number[] {
         if (flag) break;
     }
 
+    return res;
+}
+
+// 查找和为 n 的两个数 方法2  双指针
+export function findTowNumbers2(arr: number[], n: number): number[] {
+    const res: number[] = [];
+
+    const length = arr.length;
+    if (length === 0) return res;
+
+    let i = 0; // 头
+    let j = length - 1; // 尾
+
+    while (i < j) {
+        const n1 = arr[i];
+        const n2 = arr[j];
+        const sum = n1 + n2;
+
+        if (sum > n) {
+            // sum 大于 n, 则 j 要向前移动
+            j--;
+        } else if (sum < n) {
+            // sum 小于 n, 则 i 要微后移动
+            i++;
+        } else {
+            // 相等
+            res.push(n1);
+            res.push(n2);
+            break;
+        }
+    }
     return res;
 }
 
