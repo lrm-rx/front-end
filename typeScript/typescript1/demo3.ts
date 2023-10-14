a20b366c9a5ab15c74e08ae67db8cab0e5e6f002
lrm <756131502@qq.com>
2023-10-14 22:55:48 +0800

    学习ts 2023年10月14日

--- typeScript/typescript1/demo3.ts
+++ typeScript/typescript1/demo3.ts
@@ -1,10 +1,47 @@
 // 类中的泛型和泛型类型
-class DataManager {
-  constructor(private data: string[] | number[]) {}
-  getItem(index: number): string | number {
-    return this.data[index];
-  }
-}
+// class DataManager {
+//   constructor(private data: string[] | number[]) {}
+//   getItem(index: number): string | number {
+//     return this.data[index];
+//   }
+// }
 
-const data = new DataManager(["1"]);
-data.getItem(0);
+// interface Item {
+//   name: string;
+// }
+// class DataManager<T extends Item> {
+//   constructor(private data: T[]) {}
+//   getItem(index: number): string {
+//     return this.data[index].name;
+//   }
+// }
+
+// const data = new DataManager<string>(["1"]);
+// data.getItem(0);
+
+// const data = new DataManager([
+//   {
+//     name: "lpl",
+//   },
+// ]);
+
+// class DataManager<T extends number | string> {
+//   constructor(private data: T[]) {}
+//   getItem(index: number): T {
+//     return this.data[index];
+//   }
+// }
+
+// interface Test {
+//   name: string;
+// }
+
+// const data = new DataManager<Test>([]);
+
+// const data = new DataManager<string>([]);
+
+// 使用泛型作为一个具体的类型注解
+const func: <T>(param: T) => T = hello;
+function hello<T>(params: T) {
+  return params;
+}
