adb74f2907f2032c031c422602e2272115ea441e
lrm <756131502@qq.com>
2023-10-15 13:20:38 +0800

    ts命名空间

--- typeScript/typescript2/src/page.ts
+++ typeScript/typescript2/src/page.ts
@@ -1,31 +1,41 @@
-class Header {
-  constructor() {
-    const elem = document.createElement("div");
-    elem.innerText = "This is Header!!!";
-    document.body.appendChild(elem);
-  }
-}
+///<reference path='./components.ts' />
 
-class Content {
-  constructor() {
-    const elem = document.createElement("div");
-    elem.innerText = "This is Content!!!";
-    document.body.appendChild(elem);
-  }
-}
+namespace Home {
+  // class Header {
+  //   constructor() {
+  //     const elem = document.createElement("div");
+  //     elem.innerText = "This is Header!!!";
+  //     document.body.appendChild(elem);
+  //   }
+  // }
 
-class Footer {
-  constructor() {
-    const elem = document.createElement("div");
-    elem.innerText = "This is Footer!!!";
-    document.body.appendChild(elem);
-  }
-}
+  // class Content {
+  //   constructor() {
+  //     const elem = document.createElement("div");
+  //     elem.innerText = "This is Content!!!";
+  //     document.body.appendChild(elem);
+  //   }
+  // }
 
-class Page {
-  constructor() {
-    new Header();
-    new Content();
-    new Footer();
+  // class Footer {
+  //   constructor() {
+  //     const elem = document.createElement("div");
+  //     elem.innerText = "This is Footer!!!";
+  //     document.body.appendChild(elem);
+  //   }
+  // }
+  export class Page {
+    user: Components.User = {
+      name: "lpl",
+    };
+    constructor() {
+      // new Header();
+      // new Content();
+      // new Footer();
+
+      new Components.Header();
+      new Components.Content();
+      new Components.Footer();
+    }
   }
 }
