c02de7671cf1b666db9eb121a585dc09e4a66ae1
lrm <756131502@qq.com>
2023-10-25 23:03:29 +0800

    react demo

---
+++ typeScript/react-project/src/pages/Login/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { LockOutlined } from "@ant-design/icons";
+import { Button, Form, Input } from "antd";
+import "./login.css";
+
+const Login: React.FC = () => {
+  const onFinish = (values: any) => {
+    console.log("Received values of form: ", values);
+  };
+
+  return (
+    <div className="login-page">
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "请输入密码!" }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="密码"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button
+            style={{ width: "100%" }}
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            登录
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default Login;
