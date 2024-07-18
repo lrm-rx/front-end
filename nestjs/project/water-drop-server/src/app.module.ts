adf62d270a8e32418968756e27e4bdb15d514158
lrm <756131502@qq.com>
2024-07-18 23:07:24 +0800

    使用nestjs + typeorm生成数据表

--- nestjs/project/water-drop-server/src/app.module.ts
+++ nestjs/project/water-drop-server/src/app.module.ts
@@ -1,9 +1,25 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '756131502',
+      database: 'water-drop',
+      entities: [`${__dirname}/../modules/**/*.entity{.ts,.js}`],
+      logging: true,
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ nestjs/project/water-drop-server/src/modules/user/models/user.entity.ts
@@ -0,0 +1,37 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { IsNotEmpty } from 'class-validator';
+@Entity('user')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({
+    comment: '昵称',
+    default: '',
+  })
+  @IsNotEmpty()
+  name: string;
+
+  @Column({
+    comment: '描述信息',
+    default: '',
+  })
+  desc: string;
+
+  @Column({
+    comment: '手机号码',
+    nullable: true,
+  })
+  tel: string;
+
+  @Column({
+    comment: '密码',
+    nullable: true,
+  })
+  password: string;
+
+  @Column({
+    comment: '账户信息',
+    nullable: true,
+  })
+  account: string;
+}
